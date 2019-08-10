95461fee1466051bd9e918d93c02cef408684d76
John Jacobsen <john@mail.npxdesigns.com>
2019-08-09 22:37:58 -0500

    Little README tweaks

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
+# Adapted from
 # https://github.com/cicakhq/potato/blob/master/tools/build_binary.sh
-
-#  The dumped binary calls SERVER-CONFIG2:LAUNCH-SERVICE which
-#  processes the configuration file, startup options and allows the
-#  user to choose which service to start.
-
 sbcl --non-interactive \
      --disable-debugger \
      --load src/util.lisp \
